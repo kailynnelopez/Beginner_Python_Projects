6e6f8a92dc9728254e1addd2686b03b74c1be7ab
Kailynnelopez <kailynnelopez@gmail.com>
2022-11-29 16:34:30 -0800

    Framework for python project 1

---
+++ 1_MadLibs.py
@@ -0,0 +1,28 @@
+# # from SampleMadlibs import ML_Becky, ML_Brenden, ML_Dad, ML_Eli, ML_Mom
+# import uses all the files
+# import random 
+
+# if __name__ == "__main__":
+#     m = random.choice([ML_Becky, ML_Becky, ML_Mom, ML_Dad, ML_Eli])
+#     m.madlib()
+
+# # print("Select From the Following Madlibs: ")
+# # print("1. Yolanda")
+# # print("2. Manolo")
+# # print("3. Brenden")
+# # print("4. Rebecca")
+# # print("5. Eli")
+
+# # select = input("Enter Number:" )
+
+# # if select == "1":
+# #     ML_Mom()
+# # elif select == "2":
+# #     ML_Dad()
+# # elif select == "3":
+# #     ML_Brenden()
+# # elif select == "4":
+# #     ML_Becky()
+# # else:
+# #     ML_Eli()
+
---
+++ SampleMadlibs/ML_Mom.py
@@ -0,0 +1,16 @@
+# User Inputs
+year = input("Approx Year Einstein was born: ")
+adj1 = input("Adjective: ")
+smell = input("Smell: ")
+adj2 = input("Adjective: ")
+profession = input("Profession:  ")
+verb = input("Verb: ")
+adj3 = input("Adjective: ")
+famousperson = input("Famous Person: ")
+noun = input("Noun: ")
+
+#Madlib Statements 
+Mom_Madlib = f"In the year {year}, {adj1} Olivia gave birth to the {smell} smelling Yolanda. \
+    Currently, Yolanda is a {adj2} teacher.  While she may not be a great {profession}, \
+    she is excellent at {verb}. She is also a {adj3} wife.  Her organization and cleaning could put even \
+    {famousperson} to shame. {noun} is her favorite word because it reminder her of her father. "
